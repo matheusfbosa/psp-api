e51ecee17044fefeaec22452dcac8b979f459a5b
Matheus Bosa <matheusfbosa@gmail.com>
2023-11-04 23:49:48 -0300

    refactor: :fire: Remove AppController and AppService examples

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
 import { TransactionsModule } from './application/transactions/transactions.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [HealthModule, TransactionsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
