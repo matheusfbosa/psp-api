35270ae9d055777aa85b90d7e85e5c6197df072a
Matheus Bosa <matheusfbosa@gmail.com>
2023-11-05 20:41:24 -0300

    test: :white_check_mark: Add more tests to transactions.service.spec.ts

--- src/application/transactions/transactions.service.spec.ts
+++ src/application/transactions/transactions.service.spec.ts
@@ -2,27 +2,72 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../../infrastructure/database/prisma.service';
 import { TransactionsService } from './transactions.service';
 import { TransactionsRepository } from './repositories/transactions.repository';
-import { PostgresTransactionsRepository } from './repositories/postgres/postgres-transactions.repository';
+import { Transaction } from './entities/transaction.entity';
+
+const transactionFixture: Transaction = {
+  userId: 1,
+  value: 100.0,
+  description: 'Test Transaction',
+  paymentMethod: 'debit_card',
+  cardNumber: '1234567890123456',
+  cardHolder: 'John Doe',
+  cardExpiry: '12/25',
+  cvv: '123',
+  createdAt: new Date(),
+};
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
+  let repository: TransactionsRepository;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         PrismaService,
         TransactionsService,
         {
           provide: TransactionsRepository,
-          useClass: PostgresTransactionsRepository,
+          useValue: {
+            create: jest.fn(),
+            findMany: jest.fn(),
+          },
         },
       ],
     }).compile();
 
     service = module.get<TransactionsService>(TransactionsService);
+    repository = module.get<TransactionsRepository>(TransactionsRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a transaction with the last 4 digits of the card number', async () => {
+      const expected: Transaction = {
+        ...transactionFixture,
+        cardNumber: '3456',
+      };
+      (repository.create as jest.Mock).mockResolvedValueOnce(expected);
+
+      const result = await service.create(transactionFixture);
+
+      expect(result).toEqual(expected);
+      expect(repository.create).toHaveBeenCalledWith(expected);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of transactions', async () => {
+      const expected: Transaction[] = [transactionFixture];
+      (repository.findMany as jest.Mock).mockResolvedValueOnce(expected);
+
+      const result = await service.findAll();
+
+      expect(result).toEqual(expected);
+      expect(repository.findMany).toHaveBeenCalled();
+    });
   });
 });
