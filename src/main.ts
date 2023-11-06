d9291ecb733e2fa6f1f1f8566369a4551b3f1391
Matheus Bosa <matheusfbosa@gmail.com>
2023-11-05 23:45:58 -0300

    feat: :memo: Add Swagger REST API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
-  const configService = app.get(ConfigService);
+
   app.useLogger(app.get(Logger));
   app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('PSP API')
+    .setDescription('Payment Service Provider REST API')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  const configService = app.get(ConfigService);
   const port = configService.get<number>('SERVER_PORT', 3000);
   await app.listen(port);
 }
