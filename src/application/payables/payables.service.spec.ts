e16fb709d85b6651ee3d436d44c88a0cfd8a92ed
Matheus Bosa <matheusfbosa@gmail.com>
2023-11-11 15:29:14 -0300

    test: :white_check_mark: Add new unit and e2e tests

--- src/application/payables/payables.service.spec.ts
+++ src/application/payables/payables.service.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PayablesService } from './payables.service';
 import { PayablesRepository } from './repositories/payables.repository';
+import { BalancePayablesVO } from './vo/balance-payables.vo';
+import { Payable, PayableStatus } from './entities/payable.entity';
+
+const payablesFixture: Payable[] = [
+  {
+    transactionId: 1,
+    status: PayableStatus.Paid,
+    paymentDate: new Date('2023-11-01'),
+    value: 50.0,
+  },
+  {
+    transactionId: 2,
+    status: PayableStatus.WaitingFunds,
+    paymentDate: new Date('2023-11-01'),
+    value: 75.0,
+  },
+  {
+    transactionId: 3,
+    status: PayableStatus.WaitingFunds,
+    paymentDate: new Date('2023-11-01'),
+    value: 25.0,
+  },
+];
 
 describe('PayablesService', () => {
-  let service: PayablesService;
-  let repository: PayablesRepository;
+  let payablesService: PayablesService;
+  let payablesRepository: PayablesRepository;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         PayablesService,
@@ -14,18 +37,52 @@ describe('PayablesService', () => {
           provide: PayablesRepository,
           useValue: {
             create: jest.fn(),
-            findMany: jest.fn(),
+            findBy: jest.fn(),
           },
         },
       ],
     }).compile();
 
-    service = module.get<PayablesService>(PayablesService);
-    repository = module.get<PayablesRepository>(PayablesRepository);
+    payablesService = module.get<PayablesService>(PayablesService);
+    payablesRepository = module.get<PayablesRepository>(PayablesRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-    expect(repository).toBeDefined();
+    expect(payablesService).toBeDefined();
+    expect(payablesRepository).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a payable', async () => {
+      const payableToCreate = payablesFixture[0];
+      const expected: Payable = {
+        ...payableToCreate,
+        payableId: 1,
+      };
+      (payablesRepository.create as jest.Mock).mockResolvedValueOnce(expected);
+
+      const result = await payablesService.create(payableToCreate);
+
+      expect(result).toEqual(expected);
+      expect(payablesRepository.create).toHaveBeenCalledWith(payableToCreate);
+    });
+  });
+
+  describe('getBalance', () => {
+    it('should return the balance payables for a user', async () => {
+      const userId = 1;
+      const expectedBalance: BalancePayablesVO = {
+        available: 50.0,
+        waitingFunds: 100.0,
+      };
+      (payablesRepository.findBy as jest.Mock).mockResolvedValueOnce(
+        payablesFixture,
+      );
+
+      const result = await payablesService.getBalance(userId);
+
+      expect(result).toEqual(expectedBalance);
+      expect(payablesRepository.findBy).toHaveBeenCalledWith(userId);
+    });
   });
 });
--- test/transactions.e2e-spec.ts
+++ test/transactions.e2e-spec.ts
@@ -3,11 +3,30 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { TransactionsModule } from '../src/application/transactions/transactions.module';
 import { TransactionsService } from '../src/application/transactions/transactions.service';
+import { Transaction } from '../src/application/transactions/entities/transaction.entity';
 import { PaymentsService } from '../src/application/transactions/payments.service';
+import {
+  CreateTransactionDto,
+  PaymentMethodDto,
+} from '../src/application/transactions/dto/create-transaction.dto';
+
+const transactionFixture: CreateTransactionDto = {
+  userId: 1,
+  value: 100.0,
+  description: 'Smartband XYZ 3.0',
+  paymentMethod: PaymentMethodDto.DebitCard,
+  cardNumber: '4111111145551142',
+  cardHolder: 'Ozzy Osbourne',
+  cardExpiry: '12/23',
+  cvv: '737',
+};
 
 describe('TransactionsController (e2e)', () => {
   let app: INestApplication;
-  const transactionsService = { findAll: () => ['test'] };
+  const transactionsService = {
+    findAll: () => [transactionFixture],
+    create: (transaction: Transaction) => transaction,
+  };
   const paymentsService = {};
 
   beforeAll(async () => {
@@ -33,5 +52,33 @@ describe('TransactionsController (e2e)', () => {
       .get('/api/v1/transactions')
       .expect(200)
       .expect(transactionsService.findAll());
+  });
+
+  it('/api/v1/transactions (POST) - Create transaction', () => {
+    return request(app.getHttpServer())
+      .post('/api/v1/transactions')
+      .send(transactionFixture)
+      .expect(201)
+      .expect((res) => {
+        const createdTransaction = res.body;
+        expect(createdTransaction.userId).toBe(transactionFixture.userId);
+        expect(createdTransaction.value).toBe(transactionFixture.value);
+        expect(createdTransaction.description).toBe(
+          transactionFixture.description,
+        );
+        expect(createdTransaction.paymentMethod).toBe(
+          transactionFixture.paymentMethod,
+        );
+        expect(createdTransaction.cardNumber).toBe(
+          transactionFixture.cardNumber,
+        );
+        expect(createdTransaction.cardHolder).toBe(
+          transactionFixture.cardHolder,
+        );
+        expect(createdTransaction.cardExpiry).toBe(
+          transactionFixture.cardExpiry,
+        );
+        expect(createdTransaction.cvv).toBe(transactionFixture.cvv);
+      });
   });
 });
